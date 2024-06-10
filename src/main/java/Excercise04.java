d65d0bd0ece3e3fc81a424cbd57a7173e12c7448
Dương Trung Kiên <duongtrungkien@MacBook-Pro-cua-Duong.local>
2024-06-10 20:16:18 +0700

    bt-03

--- src/main/java/Excercise04.java
+++ src/main/java/Excercise04.java
@@ -27,6 +27,33 @@ public class Excercise04 {
     void question03() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập tên");
+        String name = scanner.nextLine();
+        String capitalized = name.substring(0,1).toUpperCase() + name.substring(1);
+        System.out.println(capitalized);
+    }
+
+    void question04() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập tên");
+        String name = scanner.nextLine();
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println("Kí tự thứ " + (i+1) + " là: " + name.charAt(i));
+        }
+    }
+
+    void question05() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập họ:");
+        String last = scanner.nextLine();
+        System.out.println("Nhập tên:");
+        String first = scanner.nextLine();
+        System.out.println(last.concat(" " + first));
+    }
+
+    void question06() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập tên:");
+        String name = scanner.nextLine();
 
     }
 }
--- src/main/java/Program04.java
+++ src/main/java/Program04.java
@@ -2,7 +2,10 @@ public class Program04 {
     public static void main(String[] args) {
         Excercise04 excercise04 = new Excercise04();
         // excercise04.question01();
-        excercise04.question02();
+        // excercise04.question02();
+        // excercise04.question03();
+        // excercise04.question04();
+        excercise04.question05();
     }
 
 }
