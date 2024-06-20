ee73d7435ec71bec1090ac8d8305f5d61c97427c
Dương Trung Kiên <duongtrungkien@MacBook-Pro-cua-Duong.local>
2024-06-20 19:17:42 +0700

    bt-07

--- src/main/java/Excercise02/Program.java
+++ src/main/java/Excercise02/Program.java
@@ -6,8 +6,6 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Nhập vào số a:");
-        double a = Double.parseDouble(scanner.nextLine());
-        System.out.println("Tổng của a và pi là:" + MyMath.sumQuestion1((int) a));
+
     }
 }
